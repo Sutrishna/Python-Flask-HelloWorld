74ed6c382168eea23429b67378fa220bddc67b84
Sutrishna <39561322+Sutrishna@users.noreply.github.com>
2019-02-20 16:33:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,68 +14,77 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-path = "C:/Users/Sutrishna/Desktop/Pan_Ankit.jpg"
+class PanDetails(Resource):
+   
+    def post(self):
+        
+        imageRequest = request.json['image_request']
+        storeval=bytes(imageRequest, 'utf-8')
+        with open("imageToSave.png", "wb") as fh:
+            fh.write(base64.decodebytes(storeval))
+        
+        #print(image_file)
 
-def post(self):
-    image_file = open(path, "rb").read()
+        #Reading the image file directly
+        image_file = open("imageToSave.png", "rb").read()
 
-    headers    = {'Ocp-Apim-Subscription-Key': '065bddad0ed7496d9581b555db571d42',
-              'Content-Type': 'application/octet-stream'}
-    params     = {'language': 'unk', 'detectOrientation': 'true'}
-    vision_base_url = "https://westus.api.cognitive.microsoft.com/vision/v2.0/"
-    ocr_url = vision_base_url + "ocr"
+        headers    = {'Ocp-Apim-Subscription-Key': '3b70f44a460f42a1b1c91e3541b042b5',
+                      'Content-Type': 'application/octet-stream'}
+        params     = {'language': 'unk', 'detectOrientation': 'true'}
+        vision_base_url = "https://westus.api.cognitive.microsoft.com/vision/v2.0/"
+        ocr_url = vision_base_url + "ocr"
+        response = requests.post(
+            ocr_url, headers=headers, params=params, data=image_file)
+        response.raise_for_status()
+        #print(response.text)
 
-    response = requests.post(
-        ocr_url, headers=headers, params=params, data=image_file)
-    response.raise_for_status()
-#print(response.text)
+        #Till now, I have found two formats of Pan. The below code work for the older format.
+        analysis = response.json()
+        mainArray= analysis['regions'][0]['lines']
+        nameArray = mainArray[1]['words']
+        fnameArray = mainArray[2]['words']
+        dob = mainArray[3]['words']
+        docType = mainArray[4]['words']
+        panNumber = mainArray[5]['words']
 
-#Till now, I have found two formats of Pan. The below code work for the older format.
-    analysis = response.json()
-#print(analysis)
-    mainArray= analysis['regions'][0]['lines']
+        #Till now, I have found two formats of Pan. The below code work for the newer format.
+        #NameArray = analysis['regions'][1]['lines'][0]['words']
 
-    NameArray = mainArray[1]['words']
+        Name = ""
+        FName = ""
+        DateOfBirth = ""
+        PAN = ""
+        DocType = ""
 
-    FNameArray = mainArray[2]['words']
-    DOB = mainArray[3]['words']
-    PANNumber = mainArray[5]['words']
-    docType = mainArray[4]['words']
-#Till now, I have found two formats of Pan. The below code work for the newer format.
-#NameArray = analysis['regions'][1]['lines'][0]['words']
+        for i in nameArray:
+            #Name = i['text']
+            Name += i['text'] + " "
+            
+        for i in fnameArray:
+            #Name = i['text']
+            FName += i['text'] + " "
+            
+        for i in dob:
+            #Name = i['text']
+            DateOfBirth += i['text'] + " "
+            
+        for i in panNumber:
+            #Name = i['text']
+            PAN += i['text'] + " "
+            
+        for i in docType:
+            #Name = i['text']
+            DocType += i['text'][0]    
+            
+        #result ={"Documet_Type":DocType,"Name":Name,"Father_Name":FName,"DOB":DateOfBirth,"ID":PAN};  
 
-    Name = ""
-    FName = ""
-    DateOfBirth = ""
-    PAN = ""
-    DocType = ""
-
-    for i in NameArray:
-    #Name = i['text']
-        Name += i['text'] + " "
+        if response.text is not None:
+            os.remove("imageToSave.png")
+            
+        result ={"Documet_Type":DocType,"Name":Name[:-1],"Father_Name":FName[:-1],"DOB":DateOfBirth[:-1],"ID":PAN[:-1]};   
+        return result
     
-    for i in FNameArray:
-    #Name = i['text']
-        FName += i['text'] + " "
-    
-    for i in DOB:
-    #Name = i['text']
-        DateOfBirth += i['text'] + " "
-    
-    for i in PANNumber:
-    #Name = i['text']
-        PAN += i['text'] + " "
-
-    for i in docType:
-    #Name = i['text']
-        DocType += i['text'][0]
-    
-    result ={"Documet_Type":DocType,"Name":Name[:-1],"Father_Name":FName[:-1],"DOB":DateOfBirth[:-1],"ID":PAN[:-1]};   
-    return result
-print(result)
-    
-#api.add_resource(PanDetails, '/pandetails') # Route_1
-
+api.add_resource(PanDetails, '/pandetails') # Route_1
 
 if __name__ == '__main__':
   app.run()
